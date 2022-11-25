2157b249bb909ff55045f9bb1a5839caf7c1a9a5
miahro <mikko.ahro@gmail.com>
2022-11-25 14:39:26 +0200

    invoke tasks added

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
+
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage report -m", pty=True)
+    ctx.run("coverage html", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
