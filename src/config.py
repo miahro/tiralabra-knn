8c6058f28fe78ae607056431d5d86d7f473ad096
miahro <mikko.ahro@gmail.com>
2022-12-01 10:27:52 +0200

    added output file path

--- src/config.py
+++ src/config.py
@@ -10,4 +10,17 @@ TRAIN_Y_URL = "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
 TEST_X_URL = "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
 TEST_Y_URL = "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
 
-DATAFILEPATH = os.path.join(directory,'..','data')
+DATAFILEPATH = os.path.join(directory, '..', 'data')
+
+OUTPUTFILEPATH = os.path.join(directory, '..', "data/knn_results.csv")
+
+
+# oletusparametrit käyttöliittymää varten
+parameters = {
+    "test_index_start": 0,
+    "test_index_end": 1,
+    "train_index_start": 0,
+    "train_index_end":  100,
+    "k_value": 3,
+    "layers": 4
+}
