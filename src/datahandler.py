4eb08a300b91e245f4cd822648afbbde212842fd
miahro <mikko.ahro@gmail.com>
2022-12-01 10:25:12 +0200

    write results to file added

--- src/datahandler.py
+++ src/datahandler.py
@@ -3,6 +3,10 @@ from mnistdata import Mnistdata
 from KNN import KNN
 from utils import square_dist_matrix, list_of_indices
 from timer import Timer
+from config import OUTPUTFILEPATH
+import csv
+import os
+
 
 class DataHandler:
     """ylätason luokka joka kutsuu Mnistdata luokan oliolta 
@@ -11,6 +15,7 @@ class DataHandler:
     ja käsittelee KNN olion predict -funktion toimittamat tulokset
     DataHandler ei siis suorita varsinaista lastentaa
     """
+
     def __init__(self):
         """konstruktori alustaa luokan muuttujat tyhjiksi"""
         self.X_train_point_list = None
@@ -21,8 +26,9 @@ class DataHandler:
         self.Y_test = None
         self.Y_predicted = None
         self.knn = None
-        self.filter_value = 128 #oletusarvo
+        self.filter_value = 128  # oletusarvo
         self.timer = Timer()
+        self.initialized = False
 
     def read_mnist(self):
         """lukee Mnist tietokannan"""
@@ -30,7 +36,7 @@ class DataHandler:
 
     def set_filter(self, filter_value):
         """harmaasuodattimen arvo voidaan asettaa tästä, ennen MNIST datan lukemista"""
-        self.filter_value=filter_value
+        self.filter_value = filter_value
 
     def set_parameters(self, test_index_start, test_index_end, train_index_start, train_index_end, k=3, layers=4):
         """asetetaan parametrit, joiden perusteella KNN olio luodaan"""
@@ -42,14 +48,21 @@ class DataHandler:
         self.layers = layers
 
     def init_knn(self):
-        self.X_train_point_list = self.mnist.X_train_list(start=self.train_index_start, end=self.train_index_end)
-        self.X_train_matrix = self.mnist.X_train_matrix(start=self.train_index_start, end=self.train_index_end)
-        self.X_test_point_list = self.mnist.X_test_list(start=self.test_index_start, end=self.test_index_end)
-        self.X_test_matrix = self.mnist.X_test_matrix(start=self.test_index_start, end=self.test_index_end)
-        self.Y_train = self.mnist.Y_train_labels(start=self.train_index_start, end=self.train_index_end)
-        self.Y_test = self.mnist.Y_test_labels(start=self.test_index_start, end=self.test_index_end)
-        self.knn = KNN(self.X_train_point_list, self.X_train_matrix, self.Y_train, self.X_test_point_list, self.X_test_matrix, self.k, self.layers)
-
+        self.X_train_point_list = self.mnist.X_train_list(
+            start=self.train_index_start, end=self.train_index_end)
+        self.X_train_matrix = self.mnist.X_train_matrix(
+            start=self.train_index_start, end=self.train_index_end)
+        self.X_test_point_list = self.mnist.X_test_list(
+            start=self.test_index_start, end=self.test_index_end)
+        self.X_test_matrix = self.mnist.X_test_matrix(
+            start=self.test_index_start, end=self.test_index_end)
+        self.Y_train = self.mnist.Y_train_labels(
+            start=self.train_index_start, end=self.train_index_end)
+        self.Y_test = self.mnist.Y_test_labels(
+            start=self.test_index_start, end=self.test_index_end)
+        self.knn = KNN(self.X_train_point_list, self.X_train_matrix, self.Y_train,
+                       self.X_test_point_list, self.X_test_matrix, self.k, self.layers)
+        self.initialized = True
 
     def predict(self):
         self.timer.start()
@@ -70,10 +83,21 @@ class DataHandler:
             wrong_nos = []
             for i in range(len(wrong_ind)):
                 wrong_nos.append(self.Y_test[wrong_ind[i]])
-            return {"Y_test": self.Y_test, "Y_predicted": self.Y_predicted, "correct": len(self.Y_test)-len(wrong_ind), "wrong": len(wrong_ind), "wrong_ind": wrong_ind, "wrong_nos": wrong_nos, "wrong_orig_mnist": wrong_ind_orig_mnist, "runtime": self.timer.result()}
+            return {"test_index_start": self.test_index_start, "test_index_end": self.test_index_end, "train_index_start": self.train_index_start, "train_index_end": self.train_index_end, "Y_test": self.Y_test, "Y_predicted": self.Y_predicted, "correct": len(self.Y_test)-len(wrong_ind), "wrong": len(wrong_ind), "wrong_ind": wrong_ind, "wrong_nos": wrong_nos, "wrong_orig_mnist": wrong_ind_orig_mnist, "runtime": self.timer.result(), "k-value": self.k, "filter": self.filter_value, "layers": self.layers}
 
     def write_results_to_file(self):
-        pass
-        #tämä pitää tehdä
+        print(OUTPUTFILEPATH)
+        result = self.evaluate()
+        keys = result.keys()
+        if not os.path.isfile(OUTPUTFILEPATH):
+            with open(OUTPUTFILEPATH, 'w') as csvfile:
+                csvwriter = csv.DictWriter(
+                    csvfile, fieldnames=keys, delimiter=';')
+                csvwriter.writeheader()
+
+        with open(OUTPUTFILEPATH, 'a') as csvfile:
+            csvwriter = csv.DictWriter(csvfile, fieldnames=keys, delimiter=';')
+            csvwriter.writerow(result)
+
 
 datahandler = DataHandler()
